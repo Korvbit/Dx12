aaa83d3ca5f0b3673e7cc083e67427bd24710495
Leo Wikström <wikstrom.hl@gmail.com>
2020-02-12 15:38:07 +0100

    Removed some stuff

--- gl_testbench/Renderer.cpp
+++ gl_testbench/Renderer.cpp
@@ -1,13 +1,9 @@
-#include "OpenGL/OpenGLRenderer.h"
 #include "Dx12/Dx12Renderer.h"
 #include "Renderer.h"
 
 
 Renderer* Renderer::makeRenderer(BACKEND option)
 {
-	if (option == BACKEND::GL45)
-		return new OpenGLRenderer();
-	else if (option == BACKEND::DX12)
-		return new Dx12Renderer();
+	return new Dx12Renderer();
 }
 
